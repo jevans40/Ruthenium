995139031e079e11bfaabc67bf9355e67c8b199d
Jesse Evans <jevans40@uwyo.edu>
2022-04-28 00:32:48 -0600

    Updated Sprite Factories

--- world/sprite.go
+++ world/sprite.go
@@ -1,6 +1,10 @@
 package world
 
-import "reflect"
+import (
+	"reflect"
+
+	"github.com/jevans40/Ruthenium/ruthutil"
+)
 
 type Transform struct {
 	X float32
@@ -13,6 +17,9 @@ type Transform struct {
 
 func (t Transform) GetType() reflect.Type { return reflect.TypeOf(t) }
 func (t Transform) IsComponent()          {}
+func NewTransform(xpos float32, ypos float32, zpos float32, width float32, height float32) Transform {
+	return Transform{xpos, ypos, zpos, width, height}
+}
 
 type Sprite struct {
 	X     float32
@@ -23,5 +30,12 @@ type Sprite struct {
 	Color [4]uint8
 }
 
+func NewTexturedSprite(xpos float32, ypos float32, texheight float32, texwidth float32, texmap float32, color ruthutil.Color) Sprite {
+	return Sprite{xpos, ypos, texheight, texwidth, texmap, [4]uint8{color.Red, color.Green, color.Blue, color.Alpha}}
+}
+
+func NewUntexturedSprite(color ruthutil.Color) Sprite {
+	return Sprite{0, 0, 0, 0, 0, [4]uint8{color.Red, color.Green, color.Blue, color.Alpha}}
+}
 func (t Sprite) GetType() reflect.Type { return reflect.TypeOf(t) }
 func (t Sprite) IsComponent()          {}
